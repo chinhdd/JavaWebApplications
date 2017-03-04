40f6851608094ed37667cfeb79feaa50282be268
chinhdd <chinhdd.bk@gmail.com>
2017-03-05 03:35:13 +0700

    Add file App.java and AppTest.java

---
+++ MavenTutor/0.first-non-maven-project/src/test/java/com/kaka/maven/AppTest.java
@@ -0,0 +1,13 @@
+package com.kaka.maven;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AppTest 
+{
+	@Test
+	public void testApp()
+    {
+        assertEquals(0,new App().calculateSomething());
+    }
+}
